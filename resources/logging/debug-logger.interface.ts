c5270fe1dc5f2f8a4ca5b45862142d7b9ccb8e9b
Benjamin Green <bgreen@amplience.com>
2019-12-05 11:40:44 +0000

    wip configuring typesript for project

--- resources/logging/debug-logger.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface DebugLogger {
-  debug(message: string, ...context: any[]);
-
-  info(message: string, ...context: any[]);
-
-  warn(message: string, ...context: any[]);
-
-  error(message: string, ...context: any[]);
-}
