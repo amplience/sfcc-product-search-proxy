b46f764367399971f19f5ce758d26971a87318df
Benjamin Green <bgreen@amplience.com>
2019-12-11 21:18:59 +0000

    remove uneeded js from  imports

--- endpoints/token-supplier.ts
+++ endpoints/token-supplier.ts
@@ -1,4 +1,4 @@
-import { Request } from './model/request.js';
+import { Request } from './model/request';
 import { Response } from './model/response';
 
 export type TokenSupplier = (req: Request, res: Response) => Promise<any>;
