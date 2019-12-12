7f7da15d471bb18ce8a7b942a3990e97729f6f5a
Jonny Cheetham <jcheetham@amplience.com>
2019-12-12 13:50:18 +0000

    Removed unintended changes to app.js

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = require('./router');
 const cors = require('cors');
-const bodyParser = require('body-parser');
+
 const app = express();
 
-app.use(bodyParser.json());
-app.use(cors({origin: 'null'})); 
+app.use(cors({origin: 'null'}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(function(err, req, res, next){
