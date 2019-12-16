171036f5acf2078d130147f7d36835d69dc81c5b
Benjamin Green <bgreen@amplience.com>
2019-12-16 16:03:17 +0000

    remove uneeded generated files

--- cdk-deploy.d.ts
+++
@@ -1,6 +0,0 @@
-import cdk = require('@aws-cdk/core');
-import apigateway = require('@aws-cdk/aws-apigateway');
-export declare class SFCCProductSearchServerProxyStack extends cdk.Stack {
-    constructor(scope: cdk.App, id: string, props?: cdk.StackProps);
-}
-export declare function addCorsOptions(apiResource: apigateway.IResource): void;
